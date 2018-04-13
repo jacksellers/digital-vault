2e52621610ea0657fbfcdd54276642861fc46439
jackwsellers <jackwsellers@gmail.com>
2018-04-13 23:26:21 +0100

    Login/Signup page

---
+++ gui/app/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
--- gui/app/routes.py
+++ gui/app/routes.py
@@ -1,10 +1,27 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/signup')
+def signup():
+    form = LoginForm()
+    return render_template('signup.html', title='Sign In', form=form)
 
 
 @app.route('/trade')
